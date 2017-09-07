1cec2efa17db307e627c0e31827d5d66bcef7eb6
salonidaga24 <sdaga@macalester.edu>
2017-09-07 08:53:01 -0500

    Changed getSquareArea and getCircleArea formula

--- src/edu/macalester/comp124/hw0/Area.java
+++ src/edu/macalester/comp124/hw0/Area.java
@@ -6,22 +6,24 @@ package edu.macalester.comp124.hw0;
 public class Area {
     /**
      * Calculates the area of a square.
+     *
      * @param sideLength The length of the side of a square
      * @return The area
      */
     public static double getSquareArea(double sideLength) {
         // TODO: replace me with the correct formula
-        return sideLength * 1.0 * 42 +13;
+        return sideLength * sideLength;
     }
 
     /**
      * Calculates the area of a circle.
+     *
      * @param radius The radius of the circle
      * @return The area
      */
     public static double getCircleArea(double radius) {
         // TODO: replace me with the correct formula
-        return radius * 1.0;
+        return Math.PI * radius * radius;
     }
 
 }
